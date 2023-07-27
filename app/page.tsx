2a94cdc5030aaf64f6031c2e5352731ae0d01257
jhlee <jhlee@spicusinc.com>
2023-07-27 13:40:18 +0900

    edit service

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,14 @@ import { FrameData } from '../types/types';
 
 const Home = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const voiceRef = useRef<HTMLSelectElement>(null);
+  const rateRef = useRef<HTMLInputElement>(null);
+  const pitchRef = useRef<HTMLInputElement>(null);
   const imageCache = new Map();
   let ctx: CanvasRenderingContext2D;
   let ttsAudio: HTMLAudioElement;
 
+  /*
   const loadImage = (id: string): Promise<HTMLImageElement> => {
     return new Promise(resolve => {
       const image = document.getElementById(id) as HTMLImageElement;
@@ -22,6 +26,7 @@ const Home = () => {
       };
     });
   }
+  */
 
   const loadImageBySrc = (pathname: string) => {
     if (imageCache.has(pathname)) {
@@ -38,8 +43,8 @@ const Home = () => {
     });
   }
 
-  const drawImage = async (id: string) => {
-    const image = await loadImage(id);
+  const drawImage = async (pathname: string) => {
+    const image = await loadImageBySrc(pathname);
     ctx.drawImage(image, 0, 0);
   }
 
@@ -54,12 +59,12 @@ const Home = () => {
   }
 
   const eyeBlink = async () => {
-    const leftEye = await loadImage('eye-l');
-    const rightEye = await loadImage('eye-r');
+    const leftEye = await loadImageBySrc('/images/eye-l.png');
+    const rightEye = await loadImageBySrc('/images/eye-r.png');
 
     const targetHeight = Math.floor(leftEye.height * 0.2);
-    let height = leftEye.height;
     const ANIMATION_STEP = 20;
+    let height = leftEye.height;
 
     while (height > targetHeight) {
       await sleep(ANIMATION_STEP);
@@ -76,19 +81,19 @@ const Home = () => {
 
     await sleep(75);
     clearEyes();
-    await drawImage('eye-l-closed');
-    await drawImage('eye-r-closed');
+    await drawImage('/images/eye-l-closed.png');
+    await drawImage('/images/eye-r-closed.png');
 
     await sleep(120);
     clearEyes();
-    await drawImage('eye-l');
-    await drawImage('eye-r');
+    await drawImage('/images/eye-l.png');
+    await drawImage('/images/eye-r.png');
   }
 
   const drawMouthFrame = async (id: string) => {
     const image = await loadImageBySrc(`/images/mouth-${id}.png`);
 
-    ctx.fillStyle = `rgb(90, 81, 74)`;
+    ctx.fillStyle = 'rgb(90, 81, 74)';
     ctx.fillRect(200, 165, 100, 75);
     ctx.drawImage(image, 0, 0);
   }
@@ -97,15 +102,15 @@ const Home = () => {
     if (ttsAudio) {
       ttsAudio.pause();
     }
-    ttsAudio = new Audio(`${id}.wav`);
+    ttsAudio = new Audio(`/audios/${id}.wav`);
     
-    const response = await fetch(new Request(`${id}.json`), {
+    const response = await fetch(new Request(`/jsons/${id}.json`), {
       method: 'GET',
       mode: 'no-cors'
     });
     const visemeData: FrameData[] = await response.json();
 
-    ttsAudio.ontimeupdate = (event) => {
+    ttsAudio.ontimeupdate = () => {
       const currentFrame = visemeData.find(frameData => {
         const TRANSITION_DELAY = 60;
         return frameData.offset - (TRANSITION_DELAY / 2) >= ttsAudio.currentTime * 1000;
@@ -116,17 +121,36 @@ const Home = () => {
     ttsAudio.play();
   }
 
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const min = parseInt(event.currentTarget.getAttribute('min')!);
+    const max = parseInt(event.currentTarget.getAttribute('max')!);
+    const value = parseInt(event.currentTarget.value);
+
+    if (value < min) {
+      event.currentTarget.value = min.toString();
+      return;
+    }
+    if (value > max) {
+      event.currentTarget.value = max.toString();
+      return;
+    }
+  }
+
+  const handleClick = () => {
+    const voice = voiceRef.current!.value;
+    playAudio(voice);
+  }
+
   useEffect(() => {
     const init = async () => {
       const BLINK_INTERVAL = 3500;
       ctx = canvasRef.current!.getContext('2d')!;
 
-      await drawImage('body');
-      await drawImage('eye-l');
-      await drawImage('eye-r');
-
-      const image = await loadImageBySrc('/images/mouth-0.png');
-      ctx.drawImage(image, 0, 0);
+      const imageIds = ['/images/body.png', '/images/eye-l.png', '/images/eye-r.png', '/images/mouth-0.png'];
+      imageIds.forEach(async (imageId) => {        
+        const image = await loadImageBySrc(imageId);
+        ctx.drawImage(image, 0, 0);
+      });
 
       eyeBlink();
       setInterval(() => {
@@ -137,49 +161,58 @@ const Home = () => {
   }, []);
 
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <canvas
-        id='canvas'
-        ref={canvasRef}
-        width={512}
-        height={512}
-      />
-      <div className=''>
-        <Image
-          id='body'
-          src='/images/body.png'
-          alt={''}
+    <main className='flex flex-col h-screen items-center justify-center gap-8'>
+      <div>
+        <canvas
+          id='canvas'
+          ref={canvasRef}
           width={512}
           height={512}
         />
-        <Image
-          id='eye-l'
-          src='/images/eye-l.png'
-          alt={''}
-          width={512}
-          height={512}
-        />
-        <Image
-          id='eye-l-closed'
-          src='/images/eye-l-closed.png'
-          alt={''}
-          width={512}
-          height={512}
-        />
-        <Image
-          id='eye-r'
-          src='/images/eye-r.png'
-          alt={''}
-          width={512}
-          height={512}
-        />
-        <Image
-          id='eye-r-closed'
-          src='/images/eye-r-closed.png'
-          alt={''}
-          width={512}
-          height={512}
-        />
+      </div>
+      <div className='flex flex-col w-64 gap-4'>
+        <select
+          name='voice'
+          ref={voiceRef}
+          className='border-2 border-white outline-none px-4 py-2 bg-transparent'
+          placeholder='Voice'
+        >
+          <option value='british'>British</option>
+          <option value='french'>French</option>
+          <option value='korean'>Korean</option>
+        </select>
+        <div className='flex items-center gap-4 hidden'>
+          <input
+            type='number'
+            name='rate'
+            ref={rateRef}
+            className='border-2 border-white outline-none w-full px-4 py-2 bg-transparent'
+            placeholder='Rate'
+            min={-100}
+            max={100}
+            defaultValue={0}
+            onChange={handleChange}
+          />
+          <p>%</p>
+        </div>
+        <div className='flex items-center gap-4 hidden'>
+          <input
+            type='number'
+            name='pitch'
+            ref={pitchRef}
+            className='border-2 border-white outline-none w-full px-4 py-2 bg-transparent'
+            placeholder='Pitch'
+            min={-100}
+            max={100}
+            defaultValue={0}
+            onChange={handleChange}
+          />
+          <p>%</p>
+        </div>
+        <button
+          className='border-2 border-white active:border-neutral-500 px-4 py-2'
+          onClick={handleClick}
+        >Play</button>
       </div>
     </main>
   )
